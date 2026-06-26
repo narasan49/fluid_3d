d644a873902ee5ebb37b8ac168a85b7d997f73da
Yusuke Nara <narasan49@gmail.com>
2026-06-26 21:38:20 +0900

    フリーカメラ、グリッド設置

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use bevy::{
+    camera_controller::free_camera::{FreeCamera, FreeCameraPlugin},
+    dev_tools::infinite_grid::{InfiniteGrid, InfiniteGridPlugin, InfiniteGridSettings},
+    prelude::*,
+    render::{
+        RenderPlugin,
+        settings::{Backends, RenderCreation, WgpuSettings},
+    },
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(RenderPlugin {
+                render_creation: RenderCreation::Automatic(Box::new(WgpuSettings {
+                    backends: Some(Backends::DX12),
+                    ..default()
+                })),
+                ..default()
+            }),
+            FreeCameraPlugin,
+            InfiniteGridPlugin,
+        ))
+        .add_systems(Startup, setup_dev_tools)
+        .run();
+}
+
+fn setup_dev_tools(mut commands: Commands) {
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 5.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        FreeCamera::default(),
+    ));
+
+    commands.spawn((InfiniteGrid, InfiniteGridSettings::default()));
 }
