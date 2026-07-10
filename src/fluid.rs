4729df7aea3646d655c2fa2ab28ac9550232694a
Yusuke Nara <narasan49@gmail.com>
2026-07-10 22:07:56 +0900

    初期条件の変更。

--- src/fluid.rs
+++ src/fluid.rs
@@ -58,7 +58,7 @@ fn setup_fluid_component(
     for (entity, fluid3d, transform) in &query {
         let resources = FluidResources::new(&mut images, fluid3d.resolution);
         let fluid_uniform = FluidUniform {
-            dx: 1.0,
+            dx: 0.1,
             dt: 0.0,
             rho: fluid3d.rho,
             resolution: fluid3d.resolution,
--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn main() {
 fn setup_dev_tools(mut commands: Commands) {
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(0.0, 5.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(0.0, 0.5, 2.0).looking_at(Vec3::ZERO, Vec3::Y),
         FreeCamera::default(),
     ));
 
